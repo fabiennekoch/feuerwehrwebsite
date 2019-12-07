1376d5f53c317514444302a707bc692a8edd104c
Gabri <Gabri@LAPTOP-ANTONIA>
2019-12-07 12:28:42 +0100

    PHP Projekt

--- feuerwehrwebsite/src/com/feuerwehr/servlet.java
+++ feuerwehrwebsite/src/com/feuerwehr/servlet.java
@@ -1,11 +1,18 @@
 package com.feuerwehr;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
 
 public class servlet  extends HttpServlet{
 	
@@ -15,5 +22,27 @@ public class servlet  extends HttpServlet{
 		
 	}
 	
+	private void login() {
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/messages?useSSL=false", "root", "N9K1UsHo");
+			Statement statement = conn.createStatement();
+			
+			String uname = InputBenutzername.getText();
+			
+			String sqlStr = "select * from login where benutzername = InputBenutzername + passowor = InputPassword";
+			
+			ResultSet rset = statement.executeQuery(sqlStr);
+			if(rset.first()) {
+				return true;
+			}else {
+				
+			}
+			
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
 
 }
